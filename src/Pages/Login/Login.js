e179834e8932fc32c8a6e36f2a1ba8961c7973f5
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-04-17 17:00:47 +0600

    Password Reset Complete

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,21 +1,25 @@
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useState } from 'react';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import React, { useRef, useState } from 'react';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../firebase.init';
 import SocialLogin from '../Shared/SocialLogin/SocialLogin';
 
 const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
     let errorElement;
+    const emailRef = useRef();
     const [
         signInWithEmailAndPassword,
         user,
         loading,
         error,
       ] = useSignInWithEmailAndPassword(auth);
+      const [sendPasswordResetEmail, sending, errorSendPasswordReset] = useSendPasswordResetEmail(
+        auth
+      );
       const navigate = useNavigate('');
       if(error) {
         errorElement = <p className='text-danger'>{error.message}</p>
@@ -25,6 +29,11 @@ const Login = () => {
         const email = event.target.email.value;
         const password = event.target.password.value;
         signInWithEmailAndPassword(email, password);
+      }
+      const handlePasswordReset = () => {
+        const email = emailRef.current.value;
+        sendPasswordResetEmail(email);
+        toast(`Email sent to ${email}`);
       }
       if(user) {
         navigate('/');
@@ -36,7 +45,7 @@ const Login = () => {
           Log <span className="text-warning">In</span>
         </h1>
         <form onSubmit={handleLogin} className="form">
-          <input type="email" name="email" placeholder="Email" required />
+          <input ref={emailRef} type="email" name="email" placeholder="Email" required />
           <div className="password">
             <input
               type={showPassword ? "text" : "password"}
@@ -59,6 +68,7 @@ const Login = () => {
           />
         </form>
         <p>Don't Have an account. <span><Link className='text-warning text-decoration-none' to="/signup">Sign Up</Link></span></p>
+        <p>Forgot your password. <span onClick={handlePasswordReset} style={{cursor: 'pointer'}} className='text-warning text-decoration-none'>Rest Password</span></p>
         <ToastContainer></ToastContainer>
         <SocialLogin></SocialLogin>
       </div>
